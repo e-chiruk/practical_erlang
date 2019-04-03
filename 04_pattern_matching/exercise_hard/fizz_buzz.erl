cedac7e4900d906b8715f4b16a2f6ae9a40dc6cd
e.chiruk <evgenii.chiruk@gmail.com>
2019-04-03 16:52:17 +0300

    short link get home method

--- 04_pattern_matching/exercise_hard/fizz_buzz.erl
+++ 04_pattern_matching/exercise_hard/fizz_buzz.erl
@@ -1,2 +1,14 @@
 %% Реализовать Fizz Buzz
 %% https://habr.com/ru/post/298134/
+-module(fizz_buzz).
+
+-export([init/0]).
+
+init() ->
+  init(lists:seq(1, 100), []).
+
+init([], Acc) -> lists:reverse(Acc);
+init([Head | Tail], Acc) when Head rem 3 =:= 0 andalso Head rem 5 =:= 0 -> init(Tail, ["FizzBuzz" | Acc]);
+init([Head | Tail], Acc) when Head rem 3 =:= 0 -> init(Tail, ["Fizz" | Acc]);
+init([Head | Tail], Acc) when Head rem 5 =:= 0 -> init(Tail, ["Buzz" | Acc]);
+init([Head | Tail], Acc) -> init(Tail, [Head | Acc]).
--- 05_kv/exercise/short_link.erl
+++ 05_kv/exercise/short_link.erl
@@ -1,6 +1,6 @@
 -module(short_link).
 
--export([init/0, create_short/2, get_long/2, rand_str/1]).
+-export([init/0, create_short/2, get_long/2, rand_str/1, get_home/1]).
 
 %%% module API
 
@@ -8,9 +8,15 @@ init() ->
     %% init randomizer
     <<A:32, B:32, C:32>> = crypto:strong_rand_bytes(12),
     rand:seed(exsp, {A,B,C}),
-    State = your_state_structure,
+    State = maps:new(),
     State.
 
+get_home(Link) ->
+  Regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$",
+  case re:run(Link, Regex, [{capture, first, list}]) of
+    {match, [HomeLink]} -> HomeLink;
+    nomatch -> {error, "Incorrect link"}
+  end.
 
 create_short(LongLink, State) ->
     your_result.
