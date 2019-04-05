2b16262af08614bc5c3271abb798ed33dc6a112b
e.chiruk <evgenii.chiruk@gmail.com>
2019-04-05 17:43:05 +0300

    short link done

--- 05_kv/exercise/short_link.erl
+++ 05_kv/exercise/short_link.erl
@@ -2,27 +2,39 @@
 
 -export([init/0, create_short/2, get_long/2, rand_str/1, get_home/1]).
 
+-define(SERVER, "http://hexlet.io/").
+
 %%% module API
 
 init() ->
     %% init randomizer
     <<A:32, B:32, C:32>> = crypto:strong_rand_bytes(12),
     rand:seed(exsp, {A,B,C}),
-    State = maps:new(),
-    State.
+    {#{}, #{}}.
 
 get_home(Link) ->
-  Regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/",
+  Regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}",
   case re:run(Link, Regex, [{capture, first, list}]) of
     {match, [HomeLink]} -> HomeLink;
     nomatch -> {error, "Incorrect link"}
   end.
 
 create_short(LongLink, State) ->
-    your_result.
+  {LongLinks, ShortLinks} = State,
+  case maps:find(LongLink, LongLinks) of
+    {ok, ShortLink} -> {ShortLink, State};
+    error -> ShortLink = ?SERVER ++ rand_str(8),
+             LongLinks2 = maps:put(LongLink, ShortLink, LongLinks),
+             ShortLinks2 = maps:put(ShortLink, LongLink, ShortLinks),
+             {ShortLink, {LongLinks2, ShortLinks2}}
+  end.
 
 get_long(ShortLink, State) ->
-    {error, not_found}.
+  {_, ShortLinks} = State,
+  case maps:find(ShortLink, ShortLinks) of
+    {ok, LongLink} -> {ok, LongLink};
+    error -> {error, not_found}
+  end.
 
 
 %% generates random string of chars [a-zA-Z0-9]
