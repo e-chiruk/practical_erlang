b78901c01ce08be4d162f80f9e033f9bf610ae46
e.chiruk <evgenii.chiruk@gmail.com>
2019-04-10 14:28:43 +0300

    task 7 done

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -3,4 +3,15 @@
 -export([parse/2]).
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+    Data1 = maps:fold(fun(K,V, Acc)->
+        K1 = iolist_to_binary([<<"{{">>, K, <<"}}">>]), Acc#{ K1 => V } end, #{}, Data),
+    Str1 = maps:fold(fun(K,V, Acc) ->
+        IsInt = is_integer(V),
+        IsList = is_list(V),
+        V1 = if
+            IsInt -> integer_to_binary(V);
+            IsList -> list_to_binary(V);
+            true -> V
+        end,
+        binary:replace(Acc, K, V1) end, Str, Data1),
+    iolist_to_binary(re:replace(Str1, "{{[a-zA-Z0-9]+}}", "", [global])).
