0109dfbe7d50d3a996e15c6e24e1089297f5f60e
e.chiruk <evgenii.chiruk@gmail.com>
2019-04-11 17:54:02 +0300

    task 9 done

--- 09_gen_server/exercise/chat_room_manager.erl
+++ 09_gen_server/exercise/chat_room_manager.erl
@@ -4,34 +4,71 @@
          create_room/2, remove_room/2, get_rooms/1,
          add_user/3, remove_user/3, get_users_list/2,
          send_message/4,  get_messages_history/2]).
+-export([loop/1, handle_call/2]).
 
+-record(room, {
+              id,
+              name,
+              users = [],
+              history = []
+}).
+
+-record(state, {
+                max_rooms = 5,
+                rooms = maps:new()
+}).
 
 start() ->
-    ok.
+    spawn(?MODULE, loop, [#state{}]).
+
+
+loop(State) ->
+    receive
+      {Msg, From, Ref} ->
+        {Reply, State2} = ?MODULE:handle_call(Msg, State),
+        From ! {reply, Ref, Reply},
+        ?MODULE:loop(State2);
+      stop -> ok;
+      _Any -> ?MODULE:loop(State)
+    end.
+
+call(Server, Msg) ->
+    Ref = erlang:monitor(process, Server),
+    Server ! {Msg, self(), Ref},
+    receive
+      {reply, Ref, Reply} ->
+        erlang:demonitor(Ref, [flush]),
+        Reply;
+      {'DOWN', Ref, process, Server, Reason} ->
+        {error, Reason}
+    after 5000 ->
+      erlang:demonitor(Ref, [flush]),
+      noreply
+    end.
 
 
 create_room(Server, RoomName) ->
-    ok.
+    call(Server, {create_room, RoomName}).
 
 
 remove_room(Server, RoomId) ->
-    ok.
+    call(Server, {remove_room, RoomId}).
 
 
 get_rooms(Server) ->
-    ok.
+    call(Server, {get_rooms}).
 
 
 add_user(Server, RoomId, UserName) ->
-    ok.
+    call(Server, {add_user, RoomId, UserName}).
 
 
 remove_user(Server, RoomId, UserName) ->
-    ok.
+    call(Server, {remove_user, RoomId, UserName}).
 
 
 get_users_list(Server, RoomId) ->
-    ok.
+    call(Server, {get_users_list, RoomId}).
 
 
 send_message(Server, RoomId, UserName, Message) ->
@@ -40,3 +77,65 @@ send_message(Server, RoomId, UserName, Message) ->
 
 get_messages_history(Server, RoomId) ->
     ok.
+
+
+handle_call({create_room, RoomName}, State) ->
+    #state{max_rooms = MaxRooms, rooms = Rooms} = State,
+    case maps:size(Rooms) of
+      Size when Size >= MaxRooms -> {{error, room_limit}, State};
+      _ -> RoomId = make_ref(),
+           NewRoom = #room{id = RoomId, name = RoomName},
+           {{ok, RoomId}, State#state{rooms = maps:put(RoomId, NewRoom, Rooms)}}
+    end;
+
+handle_call({remove_room, RoomId}, State) ->
+    #state{rooms = Rooms} = State,
+    case maps:find(RoomId, Rooms) of
+      {ok, _Room} -> Rooms2 = maps:remove(RoomId, Rooms),
+                     {ok, State#state{rooms = Rooms2}};
+      error -> {{error, room_not_found}, State}
+    end;
+
+handle_call({get_rooms}, State) ->
+    #state{rooms = Rooms} = State,
+    Reply = maps:fold(fun(RoomId, #room{name = RoomName}, Acc) ->
+                      [{RoomId, RoomName} | Acc]
+                      end,
+                      [], Rooms),
+    {Reply, State};
+
+handle_call({add_user, RoomId, UserName}, State) ->
+    #state{rooms = Rooms} = State,
+    case maps:find(RoomId, Rooms) of
+        {ok, Room} -> #room{users = Users} = Room,
+                      case lists:member(UserName, Users) of
+                           true -> {{error, user_is_in_room}, State};
+                           false -> Room2 = Room#room{users = [UserName | Users]},
+                                    Rooms2 = maps:update(RoomId, Room2, Rooms),
+                                    State2 = State#state{rooms = Rooms2},
+                                    {ok, State2}
+                      end;
+        error -> {{error, room_not_found}, State}
+    end;
+
+handle_call({remove_user, RoomId, UserName}, State) ->
+    #state{rooms = Rooms} = State,
+    case maps:find(RoomId, Rooms) of
+        {ok, Room} -> #room{users = Users} = Room,
+            case lists:member(UserName, Users) of
+                false -> {{error, user_not_in_room}, State};
+                true -> Room2 = Room#room{users = lists:delete(UserName, Users)},
+                        Rooms2 = maps:update(RoomId, Room2, Rooms),
+                        State2 = State#state{rooms = Rooms2},
+                        {ok, State2}
+            end;
+        error -> {{error, room_not_found}, State}
+    end;
+
+handle_call({get_users_list, RoomId}, State) ->
+    #state{rooms = Rooms} = State,
+    case maps:find(RoomId, Rooms) of
+        {ok, Room} -> #room{users = Users} = Room,
+            {{ok, Users}, State};
+        error -> {{error, room_not_found}, State}
+    end.
